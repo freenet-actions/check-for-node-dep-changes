1b01234ff5947f92e47c900e40e8894f677e45ff
Benjamin Cooper <bencooper222@gmail.com>
2019-04-23 16:14:16 -0500

    Proper comparisons

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ Toolkit.run(
       tools.exit.neutral('This is not the default branch.');
 
     const pkg = tools.getPackageJSON();
-    tools.log.info(
-      (await tools.github.repos.getContents({
-        ...tools.context.repo,
-        ref: tools.context.payload.before,
-        path: 'package.json',
-      })).data.content,
-    );
     const oldPkg = JSON.parse(
       decode(
         (await tools.github.repos.getContents({
@@ -29,13 +22,14 @@ Toolkit.run(
     );
     tools.log.info('Current package.json', pkg);
     tools.log.info('old one', oldPkg);
-    if (
-      Object.is(pkg.dependencies, oldPkg.dependencies) &&
-      Object.is(oldPkg.devDependencies, oldPkg.dependencies)
-    )
-      tools.exit.neutral('No changes to package.json');
 
-    tools.exit.success('Package.json dependencies have been changed');
+    if (!Object.is(pkg.dependencies, oldPkg.dependencies))
+      tools.exit.success('Changed prod dependencies.');
+
+    if (!Object.is(pkg.devDependencies, oldPkg.devDependencies))
+      tools.exit.success('Changed dev dependencies');
+
+    tools.exit.neutral('No changes');
   },
   { event: 'push' },
 );
